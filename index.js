ad3a8c6145d9f4de8f3964f6c5acdcdf1d8dd744
gregtux77 <34283203+gregtux77@users.noreply.github.com>
2017-12-17 16:54:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
-
 module.exports = function(sails) {
-    
-    var exec = require('./lib/exec.js');
-    
+
+    var setup = require('./lib/setup.js');
+    var connect = require('./lib/connect.js');
+    var sendCode = require('./lib/sendCode.js');
+     var exec = require('./lib/exec.js');
+
+    gladys.on('ready', function(){
+        connect();
+    });
+
     return {
-        exec: exec
+        connect: connect,
+        setup: setup,
+        sendCode: sendCode,
+          exec: exec
     };
 };
+
